41a5bf1269e8089467436b31513e4c3ae82069ef
QuocBao7928 <143020627+QuocBao7928@users.noreply.github.com>
2023-08-24 00:58:22 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
-    botToken:'<Bot Token Here>',
-    bitlyToken:'<bit.ly Token Here>'
+    botToken:'<6450302665:AAHjjHK35wEsQbohUgUdPtt1kIAbyJt2xn0>',
+    bitlyToken:'<d34c3e12914de25960b36983fc8a1961b7bd5b97>'
 };
